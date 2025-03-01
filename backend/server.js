455ec7d4ab90fb6408849ae16f540328fe7b0369
SADU VENKATESH <saduvenkatesh141205@gmail.com>
2025-03-01 18:20:54 +0530

    codebase setup

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from "express"
+import dotenv from "dotenv"
+import { connectDB } from "./config/db.js"
+
+dotenv.config()
+
+const app = express()
+
+app.get("/home",(req,res)=>{
+    res.send("Server is ready")
+})
+
+console.log(process.env.MONGO_URI);
+
+app.listen(5000,()=>{
+    connectDB()
+    console.log("server started at http://localhost:5000");
+})
+
