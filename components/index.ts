583844218afef4149cbe0c5323392b06123ad463
Tamil Adhavan S K <99407456+adhavan18@users.noreply.github.com>
2026-03-17 19:16:55 +0530

    delete

--- components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as AIChat } from './AIChat';
-export { default as CustomCursor } from './CustomCursor';
-export { default as HeroScene } from './HeroScene';
-export { default as Navigation } from './Navigation';
